89acc69aa3172fc72b879c27ffc0504f1d8c3768
Luís Massuchini <luis.massuchini@gmail.com>
2025-08-27 09:58:30 -0300

    feat: Implementa endpoint de deletar tarefas e finalizar CRUD

--- src/main/java/com/massuchini/gerenciador_tarefas/controller/TarefaController.java
+++ src/main/java/com/massuchini/gerenciador_tarefas/controller/TarefaController.java
@@ -4,6 +4,7 @@ package com.massuchini.gerenciador_tarefas.controller;
 import com.massuchini.gerenciador_tarefas.entity.Tarefa;
 import com.massuchini.gerenciador_tarefas.service.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,4 +29,33 @@ public class TarefaController {
         return ResponseEntity.status(201).body(novaTarefa);
     }
 
-}
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarefa> buscarPorId(@PathVariable Long id) {
+        try {
+            Tarefa tarefa = tarefaService.buscarPorId(id);
+            return ResponseEntity.ok(tarefa);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tarefa> atualizarTarefa(@PathVariable Long id, @RequestBody Tarefa tarefa) {
+        try {
+            Tarefa tarefaAtualizada = tarefaService.atualizarTarefa(id, tarefa);
+            return ResponseEntity.ok(tarefaAtualizada);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarTarefa(@PathVariable Long id) {
+        try {
+            tarefaService.deletarTarefa(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+}}
--- src/main/java/com/massuchini/gerenciador_tarefas/service/TarefaService.java
+++ src/main/java/com/massuchini/gerenciador_tarefas/service/TarefaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TarefaService {
@@ -19,6 +20,33 @@ public class TarefaService {
 
     public Tarefa criarTarefa(Tarefa tarefa) {
         return tarefaRespository.save(tarefa);
+    }
+
+    public  Tarefa buscarPorId(Long id) {
+        Optional<Tarefa> tarefaOptional = tarefaRespository.findById(id);
+
+        if(tarefaOptional.isPresent()) {
+            return tarefaOptional.get();
+        } else {
+            throw new RuntimeException("Tarefa não encontrada com id: " + id);
+        }
+    }
+
+    public Tarefa atualizarTarefa(Long id, Tarefa tarefaComNovosDados) {
+        Tarefa tarefaExistente = this.buscarPorId(id);
+
+        tarefaExistente.setTitulo(tarefaComNovosDados.getTitulo());
+        tarefaExistente.setDescricao(tarefaComNovosDados.getDescricao());
+        tarefaExistente.setStatus(tarefaComNovosDados.getStatus());
+
+        return tarefaRespository.save(tarefaExistente);
+    }
+
+    public void deletarTarefa(Long id) {
+        if(!tarefaRespository.existsById(id)) {
+            throw new RuntimeException("Tarefa não encontrada com id: " + id);
+        }
+        tarefaRespository.deleteById(id);
     }
 
 }
